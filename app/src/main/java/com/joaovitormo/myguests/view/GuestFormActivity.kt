e41fa9c03c60055485deae4b7320901764cf9082
João Vitor <vitormendes388@gmail.com>
2024-02-14 13:15:46 -0300

    add update guest

--- app/src/main/java/com/joaovitormo/myguests/view/GuestFormActivity.kt
+++ app/src/main/java/com/joaovitormo/myguests/view/GuestFormActivity.kt
@@ -14,6 +14,9 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityGuestFormBinding
     private lateinit var viewModel: GuestFormViewModel
+
+    private var guestId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,24 +28,35 @@ class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonSave.setOnClickListener(this)
         binding.radioPresent.isChecked = true
 
+        observe()
         loadData()
 
     }
 
+    private fun observe(){
+        viewModel.guest.observe(this) {
+            binding.editName.setText(it.name)
+            if (it.presence) {
+                binding.radioPresent.isChecked = true
+            } else {
+                binding.radioPresent.isChecked = true
+            }
+        }
+    }
     override fun onClick(v: View) {
         if (v.id == R.id.button_save) {
             val name = binding.editName.text.toString()
             val presence = binding.radioPresent.isChecked
 
-            val model = GuestModel(0,name, presence)
-            viewModel.insert(model)
+            val model = GuestModel(guestId,name, presence)
+            viewModel.save(model)
         }
     }
 
     private fun loadData() {
         val bundle = intent.extras
         if (bundle != null) {
-            val guestId = bundle.getInt(DataBaseConstants.GUEST.ID)
+            guestId = bundle.getInt(DataBaseConstants.GUEST.ID)
             viewModel.get(guestId)
 
         }
